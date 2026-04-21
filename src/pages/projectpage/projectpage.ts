33489be7dfc7fb35572d2c88ec4f888d841f0735
KemonoBAT4 <kemono.bat.4@gmail.com>
2026-04-21 23:54:34 +0200

    added application pages components

---
+++ src/pages/projectpage/projectpage.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projectpage',
+  imports: [],
+  templateUrl: './projectpage.html',
+  styleUrl: './projectpage.css',
+})
+export class Projectpage {}
