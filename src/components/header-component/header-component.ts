307bf439cc925a6390a9fc236d6449dbd1ec1a57
KemonoBAT4 <kemono.bat.4@gmail.com>
2026-04-23 03:50:21 +0200

    updated the header component with multiple features for creating a base intestation of the portfolio

--- src/components/header-component/header-component.ts
+++ src/components/header-component/header-component.ts
@@ -1,17 +1,43 @@
-import { Component, signal, computed, WritableSignal, OnInit } from '@angular/core';
+import { Component, signal, computed, WritableSignal, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
+
+// components
+import { HeaderLinkItemComponent } from '../header-link-item-component/header-link-item-component';
+
 @Component({
     selector: 'app-header-component',
+    standalone: true,
+    imports: [
+        HeaderLinkItemComponent
+    ],
     templateUrl: './header-component.html',
     styleUrl: './header-component.css',
 })
 export class HeaderComponent implements OnInit {
 
-    title: WritableSignal<string> = signal("Marco Battisti");
+    // #region    ---------- VARIABLES ---------- # //
+
+    // title
+    // NOTE: this could be a response from a service
+    title  : WritableSignal<string> = signal("Marco Battisti");
+    router : Router                 = inject(Router);
+
+    // list of header items
+    // NOTE: this is a static list, and could be also a response from a service
+    // that fetches the data from an api / database, need to think which
+    // is the best for making the edit of this webiste more easy
+    header_items_list: Array<{ label: string, href: string }>  = [
+        { label: "Home"    , href: "/home"     },
+        { label: "About"   , href: "/about"    },
+        { label: "Projects", href: "/projects" }
+    ]
+
+    // #endregion ---------- VARIABLES ---------- # //
+
 
     // #region    ---------- COMPONENT'S CONSTRUCTOR & INIT ---------- # //
 
-    constructor(private router: Router) { }
+    constructor() { }
     ngOnInit() { }
 
     // #endregion ---------- COMPONENT'S CONSTRUCTOR & INIT ---------- # //
