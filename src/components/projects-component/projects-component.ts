b83f590793ee442db6cfc46e7c0babff7106322f
KemonoBAT4 <kemono.bat.4@gmail.com>
2026-05-01 22:39:32 +0200

    updated the projects component with multiple updates on styles and html

--- src/components/projects-component/projects-component.ts
+++ src/components/projects-component/projects-component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, input, WritableSignal, signal } from '@angular/core';
+import { Project, ProjectTag } from '../../interfaces/projects';
 
 @Component({
-  selector: 'app-projects-component',
-  imports: [],
-  templateUrl: './projects-component.html',
-  styleUrl: './projects-component.css',
+    selector: 'app-projects-component',
+    imports: [],
+    templateUrl: './projects-component.html',
+    styleUrl: './projects-component.css',
 })
-export class ProjectsComponent {}
+export class ProjectsComponent {
+    project = input.required<Project>();
+    index = input.required<number>();
+
+    imageError: WritableSignal<boolean> = signal(false);
+
+    onImageError() {
+        this.imageError.set(true);
+    }
+}
